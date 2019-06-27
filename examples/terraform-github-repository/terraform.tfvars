e0e31b93c191ed728bc388198978c9191a42d0ae
James Woolfenden <James.Woolfenden@gmail.com>
2019-06-27 08:38:38 +0100

    Draft

---
+++ examples/terraform-github-repository/terraform.tfvars
@@ -0,0 +1,8 @@
+description    = ""
+name           = "terraform-github-repository"
+private        = "false"
+has_wiki       = "true"
+has_downloads  = "true"
+has_issues     = "true"
+team_permission= "push"
+team_name      = "cds"
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type="string"
+}
+
+variable "private" {
+  type="string"
+}
+
+variable "description" {
+  type="string"
+}
+
+variable "has_wiki" {
+  type="string"  
+}
+
+variable "has_downloads" {
+  type="string"
+}
+
+variable "has_issues" {
+  type="string"
+}
+
+variable "team_permission" {
+  type="string"
+}
+
+variable "team_name" {
+  type="string"
+}
