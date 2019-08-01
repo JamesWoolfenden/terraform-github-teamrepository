5eae5e3c22c50b29354e2ad6c9a06e687ecef46b
Myles Harding <myles.harding@slalom.com>
2019-08-01 09:22:40 +0100

    remove comma

--- github_branch_protection.repositories.tf
+++ github_branch_protection.repositories.tf
@@ -8,7 +8,7 @@ resource "github_branch_protection" "repository" {
   }
 
   required_pull_request_reviews {
-    dismiss_stale_reviews = true,
+    dismiss_stale_reviews = true
     required_approving_review_count = 1
   }
 }
