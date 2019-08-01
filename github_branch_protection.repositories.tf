e0026fcc5638e4806f882dbbbf002d00c069cf6a
Myles Harding <myles.harding@slalom.com>
2019-08-01 09:21:06 +0100

    * make status checks strict * make sure at least 1 review approval is present

--- github_branch_protection.repositories.tf
+++ github_branch_protection.repositories.tf
@@ -3,7 +3,12 @@ resource "github_branch_protection" "repository" {
   branch         = "master"
   enforce_admins = true
 
+  required_status_checks {
+    strict = true
+  }
+
   required_pull_request_reviews {
-    dismiss_stale_reviews = true
+    dismiss_stale_reviews = true,
+    required_approving_review_count = 1
   }
 }
